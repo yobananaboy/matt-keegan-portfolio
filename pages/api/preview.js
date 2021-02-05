5fcd1d9109913fe9b22421e7f69236130b97775f
Matt <mattkeegan@btinternet.com>
2021-02-05 19:34:06 +0000

    Fix homepage preview behaviour

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -21,7 +21,11 @@ export default async function handler (req, res) {
 
     // Fetch the headless CMS to check if the provided `slug` exists
     // getPostBySlug would implement the required fetching logic to the headless CMS
-    let post = await getSinglePostWithSlug(query.slug, true, query.contentType)
+    let post = "homepage"
+    if(req.query.slug !== "/") {
+        let post = await getSinglePostWithSlug(query.slug, true, query.contentType)    
+    }
+    
   
     // If the slug doesn't exist prevent preview mode from being enabled
     if (!post) {
