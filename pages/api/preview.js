0f788dac8eea920a21dc84f7afcb16dad46bbc00
Matt <mattkeegan@btinternet.com>
2021-02-06 10:37:08 +0000

    Refactor pages/api/preview.js to pull content as well as slug from req.query

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -3,40 +3,26 @@ import { getSinglePostWithSlug } from '../../utils/contentfulPosts'
 export default async function handler (req, res) {
     // Check the secret and next parameters
     // This secret should only be known to this API route and the CMS
-    if (req.query.secret !== process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN || !req.query.slug) {
+    if (req.query.secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !req.query.slug) {
       return res.status(401).json({ message: 'Invalid token' })
     }
     
-    let query = {}
-    if(req.query.slug === "/") {
-        query.slug = "/"
-        query.contentType = "siteInfo"
-    } else {
-        let string = req.query.slug
-        let re = /\/(.*)\/(.*)/ // regex for matching /<post>/<slug> url format
-        let match = string.match(re)
-        query.contentType = match[1]
-        query.slug = match[2]
-    }
-
     // Fetch the headless CMS to check if the provided `slug` exists
-    // getPostBySlug would implement the required fetching logic to the headless CMS
-    let post = "homepage"
-    if(req.query.slug !== "/") post = await getSinglePostWithSlug(query.slug, true, query.contentType)    
+    // getSinglePostWithSlug would implement the required fetching logic to the headless CMS
+    let post = await getSinglePostWithSlug(req.query.slug, true, req.query.content)    
     
   
     // If the slug doesn't exist prevent preview mode from being enabled, unless it's the homepage
-    if (!post && req.query.slug !== "/") {
+    if (!post) {
       return res.status(401).json({ message: 'Invalid slug' })
     }
     // Enable Preview Mode by setting the cookies
     res.setPreviewData({})
   
+    let contentTypeSlug = req.query.content === "siteInfo" ? "" : `/${req.query.content}`
+    let postSlug = req.query.content === "siteInfo" ? "/" : `/${req.query.slug}`
+
     // Redirect to the path from the fetched post
     // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-    if(req.query.slug === "/") {
-        res.redirect("/")
-    } else {
-        res.redirect(`/${query.contentType}/${post.slug}`)
-    }
+    res.redirect(`${contentTypeSlug}${postSlug}`)
 }
