0161b5ff0eeed06a61e7e37efbc7fd5d5bcb0b71
Matt <mattkeegan@btinternet.com>
2021-03-04 17:44:18 +0000

    Fix Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -16,10 +16,6 @@ class MyDocument extends Document {
           {/* End Google Tag Manager */}
         </Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WKNPDDL"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
